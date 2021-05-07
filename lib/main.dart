cc7ce4baa94bbf7e113f26bcad33dfb2f2d05822
Danielherresan <danielherresan@gmail.com>
2021-05-07 14:28:51 -0500

    subiendo

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_academy/pages/animated_container_properties.dart';
 import 'package:flutter_academy/pages/page_template_go.dart';
 import 'package:flutter_academy/pages/physics_simulation.dart';
 
 main() {
   runApp(MaterialApp(
-    home: PhysicsCardDragDemo(),
+    home: AnimatedContainerApp(),
   ));
 }
