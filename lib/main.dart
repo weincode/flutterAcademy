892486563b90b7940625681663dcd16ed8bb6090
etoledo <etoledo@bancolombia.com.co>
2021-05-11 14:25:11 -0500

    Orientation grid

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,10 @@ import 'package:flutter_academy/pages/animations/fade_in_out.dart';
 import 'package:flutter_academy/pages/animations/page_template_go.dart';
 import 'package:flutter_academy/pages/animations/physics_simulation.dart';
 import 'package:flutter_academy/pages/design/drawer2screen.dart';
+import 'package:flutter_academy/pages/design/ui_based_orientation.dart';
 
 main() {
   runApp(MaterialApp(
-    home: Drawer2Screen()
+    home: OrientationList(title: 'UI based orientation',)
   ));
 }
